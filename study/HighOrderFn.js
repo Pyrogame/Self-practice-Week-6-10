76eda796d6e5345d6cf3cda7ed57ac18153b0954
Pyrogame <waramate.ta@gmail.com>
2025-09-16 16:57:21 +0700

    study week6

--- study/HighOrderFn.js
+++ study/HighOrderFn.js
@@ -18,3 +18,47 @@ function getScore(x) {
 const score = getScore(1)
 console.log(score)
 // console.log(x, mid, final) //cannot access
+
+const z = 1
+function makeAdder(x){
+  console.log(x)
+  // console.log(y) // y is not defined
+  console.log(z)
+  return function doSomething(y) {
+    return x + y + z
+  }
+}
+const result = makeAdder(10)
+console.log(typeof result)
+console.log(result(3))
+
+/// Practice-1
+function idGenerator(){
+  let idcount = 0
+  function generator(){
+    return idcount += 1
+  }
+  return generator
+}
+
+const idGen = idGenerator()
+
+console.log(idGen()) // 1
+console.log(idGen()) // 2
+console.log(idGen()) // 3
+
+
+///Practice-2
+
+function outerFunction(x){
+   return function innerFunction(y){
+    return x + y
+   }
+}
+
+const addFive = outerFunction(5)
+console.log(addFive(3)) //8
+
+const addTen = outerFunction(10)
+console.log(addTen(2)) //12
+
